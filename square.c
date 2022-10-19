9fc476a977e280711e76ff43bb9370346b94539d
jivandandapat07 <jivandandapat07@gmail.com>
2022-10-19 23:22:25 +0530

    Added a code

---
+++ square.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+float squareArea(float side);
+float circleArea(float rad);
+float rectangleArea(float a, float b);
+
+int main(){
+   
+   float side=5.0;
+   
+   
+   printf("area is:%f",squareArea(side));
+    return 0;
+}
+float squareArea(float side){
+    return side*side;
+}
+float circleArea(float rad){
+    return 3.14*rad*rad;
+}
+float rectangleArea(float a, float b){
+    return a*b;
+}
